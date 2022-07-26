1de967c60f16c153cb4adf11522efa525d3a76cd
Fu Zi Xiang <speed2exe@live.com.sg>
2022-07-26 23:30:50 +0800

    minor rework + testing

--- lib.zig
+++ lib.zig
@@ -1,101 +1,76 @@
 const std = @import("std");
+const info = std.log.info;
 
 // add default value based on type field
 pub fn FlagOf(comptime T: type) type {
     return struct {
         const Self = @This();
 
-        name:      []const u8,
-        parseFunc: fn([]const u8) anyerror!T,
-        default:   ?T                       = null,
-        alias:     ?[]const u8              = null,
-        desc:      ?[]const u8              = null,
-
-        pub fn init(self: Self) FlagResult(T) {
-            return FlagResult(T){
-                .parseFunc = self.parseFunc,
-            };
-        }
+        name:       []const u8,
+        parse_func: fn([]const u8) anyerror!T,
+        default:    ?T                       = null,
+        alias:      ?[]const u8              = null,
+        desc:       ?[]const u8              = null,
     };
 }
 
-// Need to test
-fn FlagResult(comptime T: type) type {
-    return struct {
-        const Self = @This();
-
-        strValue: ?[]const u8 = null,
-        parseFunc : fn([]const u8) anyerror!T,
-        evaluated: bool = false,
-        value: anyerror!?T = null,
-
-        pub fn eval(self: *Self) anyerror!?T {
-            if (self.evaluated) {
-                return self.value;
-            }
-            defer {
-                self.evaluated = true;
-            }
-            var str: []const u8 = self.strValue orelse return null;
-            const parsed: T = try self.parseFunc(str);
-            defer {
-                self.value = parsed;
-            }
-            return @as(anyerror!?T, parsed);
-        }
-
-        // need a funtion to register with os orgs,
-        // so that the strValue can be set when initialized
-    };
-}
-
-fn parsei8(str: []const u8) !i8 {
-    return std.fmt.parseInt(i8, str, 10);
-}
-
-test "test FlagResult 1" {
-    var flag_result = FlagResult(i8) {
-        .strValue = "9",
-        .parseFunc = parsei8,
-        .evaluated = false,
-        .value = null,
-    };
-
-    const optValue = try flag_result.eval();
-    const value: i8 = optValue orelse unreachable;
-    
-
-    try std.testing.expect(value == @as(i8, 9));
-    try std.testing.expect(flag_result.evaluated == true);
-    const flagValOpt: ?i8 = flag_result.value catch unreachable;
-    const flagVal: i8 = flagValOpt orelse unreachable;
-    try std.testing.expect(flagVal == @as(i8, 9));
-}
-
-
-// Provided by flag so that when args is parsed, the registered flag will 
-// automatically assigned the string(not yet parsed)
-// TODO: investigate into comptime hashmap, for now just do simple loop
-// Behavior: if multiple args with same name is set, it will only apply the latest
-const registration = struct {
-    alias: []const u8,
-    name: []const u8,
-    setFunc: fn([]const u8) void,
-};
-
-var os_kv_map: OsArgsKV = undefined;
-
-const OsArgsKV = struct {
-
-    // e.g. some_app -n 12 => n is the key, 12 is the value to be passed in to
-    // the set_func
-    set_func_by_arg: std.HashMap([]const u8, fn([]const u8)void),
-    
-    // TODO: stored unmapped values??
-
-    pub fn init() void {
-        // get all args from os program args 
-        // iterate, if exists, set the value using set_func
+pub fn getOsArgKeyValue() !std.StringHashMap([]const u8) {
+    var osKeyValue = std.StringHashMap([]const u8).init(std.heap.page_allocator);
+    var os_args_consumer = OsArgsConsumer{};
+    while (os_args_consumer.consume()) |result| {
+        _ = result;
     }
 
+    return osKeyValue;
+}
+
+const KeyValue = struct {
+    key: []const u8,
+    value: ?[]const u8,
 };
+
+const OsArgsConsumer = struct {
+    os_args: [][*:0]const u8,
+
+    fn popFromStart(self: *OsArgsConsumer) [*:0]const u8 {
+        defer self.os_args = self.os_args[1..];
+        return self.os_args[0];
+    }
+
+    fn consume(self: *OsArgsConsumer) ?KeyValue {
+        if (self.os_args.len == 0) {
+            return null;
+        }
+        
+        
+
+        const next = self.popFromStart();
+        _ = next;
+        
+        return null;
+    }
+};
+
+// example: --number=8
+fn equal(str: [*:0]const u8) ?KeyValue {
+    _ = str;
+    return null;
+}
+
+pub fn consumeOsArgs(args: [][*:0]const u8) void {
+    _ = args;
+}
+
+pub fn main() !void {
+    info("return: {}", .{returnOptErr()});
+    
+    while (returnOptErr()) |value| {
+        info("in while loop value: {}", .{value});
+    }
+
+}
+
+fn returnOptErr() ?i8 {
+    return 8;
+}
+
--- main.zig
+++ main.zig
@@ -2,17 +2,19 @@ const std = @import("std");
 const lib = @import("./lib.zig");
 
 pub fn main() void {
+
     // comptime known
     const I8FlagType = lib.FlagOf(i8);
-    const num_flag: I8FlagType = .{.name = "number", .parseFunc = parsei8};
+    var num_flag: I8FlagType = .{.name = "number", .parseFunc = parsei8};
 
     std.log.info("{s}", .{@TypeOf(num_flag)});
     std.log.info("{s}", .{num_flag});
 
-    var inited = num_flag.init();
-    std.log.info("{s}", .{@TypeOf(inited)});
-    std.log.info("{s}", .{inited});
 
+
+    // 
+
+    const i8value: i8 = num_flag.init().get();
 }
 
 fn parsei8(str: []const u8) !i8 {
