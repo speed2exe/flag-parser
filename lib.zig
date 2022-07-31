1dbcee8558027f6137334fefdd7251b17e45ebbe
Fu Zi Xiang <speed2exe@live.com.sg>
2022-07-31 22:58:10 +0800

    added test cases for os args kv

--- lib.zig
+++ lib.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const print = std.debug.print;
 const mem = std.mem;
 const expect = std.testing.expect;
+const expectError = std.testing.expectError;
 const info = std.log.info;
 
 // add default value based on type field
@@ -17,9 +18,8 @@ pub fn FlagOf(comptime T: type) type {
     };
 }
 
-pub fn keyValueFromArgs(args: [][*:0]const u8) !std.StringHashMap([]const u8) {
-    // TODO: allow allocator to be passed in
-    var key_value = std.StringHashMap([]const u8).init(std.heap.page_allocator);
+pub fn keyValueFromArgs(allocator: std.mem.Allocator,  args: [][*:0]const u8) !std.StringHashMap([]const u8) {
+    var key_value = std.StringHashMap([]const u8).init(allocator);
     var os_args_consumer = ArgsConsumer{.args = args};
     while (try os_args_consumer.consume()) |result| {
         try key_value.put(result.key, result.value);
@@ -73,6 +73,26 @@ const ArgsConsumer = struct {
         return KeyValue{.key = next, .value = next2};
     }
 };
+
+test "keyValueFromArgs" {
+    var args = [_][*:0]const u8{"--hello", "world"};
+    var kv: std.StringHashMap([]const u8) = try keyValueFromArgs(std.heap.page_allocator, &args);
+    var value = kv.get("hello").?;
+    try expect(std.mem.eql(u8, value, "world"));
+
+    args = [_][*:0]const u8{"-h", "world"};
+    kv = try keyValueFromArgs(std.heap.page_allocator, &args);
+    value = kv.get("h").?;
+    try expect(std.mem.eql(u8, value, "world"));
+
+    args = [_][*:0]const u8{"-h", " "};
+    kv = try keyValueFromArgs(std.heap.page_allocator, &args);
+    value = kv.get("h").?;
+    try expect(std.mem.eql(u8, value, " "));
+
+    args = [_][*:0]const u8{"hello", " "};
+    try expectError(error.InvalidArgs, keyValueFromArgs(std.heap.page_allocator, &args));
+}
 
 fn getEqualityKeyValue(str: []const u8) ?KeyValue {
     for (str) |b, i| {
@@ -134,11 +154,13 @@ test "startWithDash" {
 }
 
 pub fn main() !void {
-    var args = [_][*:0]const u8{"--hello", "world"};
-    var args_ptr: [][*:0]const u8 = &args;
-    var kv: std.StringHashMap([]const u8) = try keyValueFromArgs(args_ptr);
-    const value = kv.get("hello").?;
-    print("value from hello key: {s}", .{value});
+    var args = [_][*:0]const u8{"help", " "};
+    if (keyValueFromArgs(std.heap.page_allocator, &args)) |kv| {
+        _ = kv;
+        print("success!", .{});
+    } else |err| {
+        print("failed! {}", .{err});
+    }
 }
 
 fn sliceFromSentinel() []const u8{
