343932c01c56ebf73967ab4c211260b90e9958bc
Fu Zi Xiang <speed2exe@live.com.sg>
2022-07-31 23:31:10 +0800

    auto save + macro for everyline in visual mode

--- main.zig
+++ main.zig
@@ -2,21 +2,46 @@ const std = @import("std");
 const lib = @import("./lib.zig");
 
 pub fn main() void {
-
     // comptime known
-    const I8FlagType = lib.FlagOf(i8);
-    var num_flag: I8FlagType = .{.name = "number", .parseFunc = parsei8};
+    comptime {
+        const I8FlagType = Flag(i8);
+        const num_flag = I8FlagType.init("number", parsei8);
+        _ = num_flag;
 
-    std.log.info("{s}", .{@TypeOf(num_flag)});
-    std.log.info("{s}", .{num_flag});
+        @compileLog("hello", {});
+    }
 
-
-
-    // 
-
-    const i8value: i8 = num_flag.init().get();
+    // const num_flag = I8FlagType.init("number", parsei8).withAlias("num");
+    // std.log.info("{s}", .{@TypeOf(num_flag)});
+    // std.log.info("{s}", .{num_flag});
 }
 
 fn parsei8(str: []const u8) !i8 {
     return std.fmt.parseInt(i8, str, 10);
 }
+
+pub fn Flag(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        name:       []const u8,
+        parse_func: fn([]const u8) anyerror!T,
+        default:    ?T                       = null,
+        alias:      ?[]const u8              = null,
+        desc:       ?[]const u8              = null,
+
+        pub fn init(name: []const u8, parse_func: fn([]const u8) anyerror!T) *Self {
+            return &Self {
+                .name = name,
+                .parse_func = parse_func,
+            };
+        }
+
+        pub fn withAlias(self: *Self, alias: []const u8) *Self {
+            self.alias = alias;
+            return self;
+        }
+    };
+}
+
+
