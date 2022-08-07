6fe5c9cdd6dd8da4f0ef51f522e19b4359d8200e
Fu Zi Xiang <speed2exe@live.com.sg>
2022-08-07 23:06:24 +0800

    added funtionlity to check for flag with empty value

--- lib.zig
+++ lib.zig
@@ -16,11 +16,11 @@ pub fn Flag(comptime T: type) type {
     return struct {
         const Self = @This();
 
-        name:       []const u8,
-        parse_func: fn([]const u8) anyerror!T,
-        default:    ?T                       = null,
-        alias:      ?[]const u8              = null,
-        desc:       ?[]const u8              = null,
+        name: []const u8,
+        parse_func: fn (?[]const u8) anyerror!T,
+        default: ?T = null,
+        alias: ?[]const u8 = null,
+        desc: ?[]const u8 = null,
 
         pub fn init(self: Self) Self {
             //TODO:
@@ -30,7 +30,7 @@ pub fn Flag(comptime T: type) type {
             return self;
         }
 
-        pub fn valueFromMap(self: Self, key_value: std.StringHashMap([]const u8)) anyerror!?T {
+        pub fn valueFromMap(self: Self, key_value: std.StringHashMap(?[]const u8)) anyerror!?T {
             const opt_named_value = key_value.get(self.name);
             const opt_aliased_value = blk: {
                 if (self.alias) |alias_name| {
@@ -54,8 +54,7 @@ pub fn Flag(comptime T: type) type {
             }
 
             return self.default;
-       }
-
+        }
     };
 }
 
@@ -64,12 +63,12 @@ pub fn Flag(comptime T: type) type {
 // wait for errors with value?
 // return the rest of the args after - or --
 // wait for tuples with multiple return values?
-pub fn keyValueFromArgs(allocator: std.mem.Allocator, args: [][*:0]const u8) !std.StringHashMap([]const u8) {
-    var key_value = std.StringHashMap([]const u8).init(allocator);
-    var os_args_consumer = ArgsConsumer{.args = args};
+pub fn keyValueFromArgs(allocator: std.mem.Allocator, args: [][*:0]const u8) !std.StringHashMap(?[]const u8) {
+    var key_value = std.StringHashMap(?[]const u8).init(allocator);
+    var os_args_consumer = ArgsConsumer{ .args = args };
     while (try os_args_consumer.consume()) |result| {
         if (key_value.getKey(result.key)) |key| {
-            std.log.err("duplicated args found: {s}\n",.{key});
+            std.log.err("duplicated args found: {s}\n", .{key});
             return error.DuplicatedArguments;
         }
         try key_value.put(result.key, result.value);
@@ -77,9 +76,32 @@ pub fn keyValueFromArgs(allocator: std.mem.Allocator, args: [][*:0]const u8) !st
     return key_value;
 }
 
+// const KeyValueAndParams = struct {
+//     // key_value represents the parsed result from e.g.:
+//     // --name hello --line 12
+//     key_value: std.StringHashMap([]const u8),
+//
+//     params: [][*:0]const u8,
+// };
+//
+// pub fn keyValueAndParamsFromArgs(allocator: std.mem.Allocator, args: [][*:0]const u8) !KeyValueAndParams {
+//     var key_value = std.StringHashMap([]const u8).init(allocator);
+//     var os_args_consumer = ArgsConsumer{.args = args};
+//
+//     while (try os_args_consumer.consume()) |result| {
+//         if (key_value.getKey(result.key)) |key| {
+//             std.log.err("duplicated args found: {s}\n",.{key});
+//             return error.DuplicatedArguments;
+//         }
+//         try key_value.put(result.key, result.value);
+//     }
+//
+//     return key_value;
+// }
+
 const KeyValue = struct {
     key: []const u8,
-    value: []const u8,
+    value: ?[]const u8,
 };
 
 const ArgsConsumer = struct {
@@ -121,55 +143,63 @@ const ArgsConsumer = struct {
             return result;
         }
 
-        const next2 = self.peekFromStart() orelse return KeyValue{.key = next, .value = ""};
+        const next2 = self.peekFromStart() orelse return KeyValue{ .key = next, .value = null };
         if (startWithDash(next2)) {
-            return KeyValue{.key = next, .value = ""};
+            return KeyValue{ .key = next, .value = null };
         }
 
         self.args = self.args[1..];
-        return KeyValue{.key = next, .value = next2};
+        return KeyValue{ .key = next, .value = next2 };
     }
 };
 
 test "keyValueFromArgs" {
     // normal passing case
-    var args = [_][*:0]const u8{"--hello", "world"};
-    var kv: std.StringHashMap([]const u8) = try keyValueFromArgs(std.heap.page_allocator, &args);
+    var args = [_][*:0]const u8{ "--hello", "world" };
+    var kv: std.StringHashMap(?[]const u8) = try keyValueFromArgs(std.heap.page_allocator, &args);
     var value = kv.get("hello").?;
-    try expect(std.mem.eql(u8, value, "world"));
+    try expect(std.mem.eql(u8, value.?, "world"));
 
     // with single dash
-    args = [_][*:0]const u8{"-h", "world"};
+    args = [_][*:0]const u8{ "-h", "world" };
     kv = try keyValueFromArgs(std.heap.page_allocator, &args);
     value = kv.get("h").?;
-    try expect(std.mem.eql(u8, value, "world"));
+    try expect(std.mem.eql(u8, value.?, "world"));
 
     // with next args as blank
-    args = [_][*:0]const u8{"-h", " "};
+    args = [_][*:0]const u8{ "-h", " " };
     kv = try keyValueFromArgs(std.heap.page_allocator, &args);
     value = kv.get("h").?;
-    try expect(std.mem.eql(u8, value, " "));
+    try expect(std.mem.eql(u8, value.?, " "));
+
+    // with no value
+    args = [_][*:0]const u8{ "-h", "-l" };
+    kv = try keyValueFromArgs(std.heap.page_allocator, &args);
+    value = kv.get("h").?;
+    try expect(value == null);
+    value = kv.get("l").?;
+    try expect(value == null);
 
     // with terminating - or --
-    var args2 = [_][*:0]const u8{"--number", "1", "--lines", "2", "--", "my_path", "another_arg"};
+    var args2 = [_][*:0]const u8{ "--number", "1", "--lines", "2", "--", "my_path", "another_arg" };
     info("args2: {s}", .{args2});
     kv = try keyValueFromArgs(std.heap.page_allocator, &args2);
     value = kv.get("number").?;
-    try expect(std.mem.eql(u8, value, "1"));
+    try expect(std.mem.eql(u8, value.?, "1"));
     value = kv.get("lines").?;
-    try expect(std.mem.eql(u8, value, "2"));
+    try expect(std.mem.eql(u8, value.?, "2"));
 
     // expected to fail
-    args = [_][*:0]const u8{"hello", " "};
+    args = [_][*:0]const u8{ "hello", " " };
     try expectError(error.InvalidArgs, keyValueFromArgs(std.heap.page_allocator, &args));
 }
 
 fn getEqualityKeyValue(str: []const u8) ?KeyValue {
     for (str) |b, i| {
         if (b == '=') {
-            return KeyValue {
+            return KeyValue{
                 .key = str[0..i],
-                .value = str[i+1..],
+                .value = str[i + 1 ..],
             };
         }
     }
@@ -182,17 +212,16 @@ test "getEqualityKeyValue" {
 
     const kv: KeyValue = getEqualityKeyValue("hello=world").?;
     try expect(mem.eql(u8, kv.key, @as([]const u8, "hello")));
-    try expect(mem.eql(u8, kv.value, @as([]const u8, "world")));
+    try expect(mem.eql(u8, kv.value.?, @as([]const u8, "world")));
 
     const kv2: KeyValue = getEqualityKeyValue("hello=").?;
     try expect(mem.eql(u8, kv2.key, @as([]const u8, "hello")));
-    try expect(mem.eql(u8, kv2.value, @as([]const u8, "")));
+    try expect(mem.eql(u8, kv2.value.?, @as([]const u8, "")));
 
     const kv3: KeyValue = getEqualityKeyValue("=world").?;
     try expect(mem.eql(u8, kv3.key, @as([]const u8, "")));
-    try expect(mem.eql(u8, kv3.value, @as([]const u8, "world")));
+    try expect(mem.eql(u8, kv3.value.?, @as([]const u8, "world")));
 }
-
 
 fn trimDash(str: []const u8) []const u8 {
     for (str) |b, i| {
--- main.zig
+++ main.zig
@@ -11,9 +11,10 @@ pub fn main() !void {
     const kv = try lib.keyValueFromArgs(std.heap.page_allocator, lib.getOsArgs());
 
     // Declare the args:
-    // TODO: ensure that each flag declared is not duplicated, name or alias, throw error during comptime
-    const err_opt_num = (lib.Flag(i8){.name = "number", .parse_func = parsei8}).init().valueFromMap(kv);
-    const line = (lib.Flag(i8){.name = "line", .parse_func = parsei8}).init().valueFromMap(kv);
+    const err_opt_num = (lib.Flag(i8){ .name = "number", .parse_func = parsei8 }).init().valueFromMap(kv);
+    const line = (lib.Flag(i8){ .name = "line", .parse_func = parsei8 }).init().valueFromMap(kv);
+    const ignore_hidden = (lib.Flag(i8){ .name = "ignore_hidden", .parse_func = parseBool }).init().valueFromMap(kv);
+
     // With Error Checking:
     // const num = try (lib.Flag(i8){.name = "number", .parse_func = parsei8}).valueFromMap(kv);
     // const line = try (lib.Flag(i8){.name = "line", .parse_func = parsei8}).valueFromMap(kv);
@@ -21,48 +22,62 @@ pub fn main() !void {
     // handling of the variable
     if (err_opt_num) |opt_num| {
         if (opt_num) |num| {
-            print("number: {}, type: {}\n", .{num,@TypeOf(num)});
+            print("number: {}, type: {}\n", .{ num, @TypeOf(num) });
         } else {
-            print("number is null ",.{});
+            print("number is null ", .{});
         }
     } else |err| {
-        print("got error while attempting to get value of number arg: {}",.{err});
+        print("got error while attempting to get value of number arg: {}", .{err});
     }
 
-    // debug your arg 
-    print("\nline: {}, type: {}\n", .{line,@TypeOf(line)});
-
-    
+    // debug your arg
+    print("\nline: {}, type: {}\n", .{ line, @TypeOf(line) });
+    print("\nignore_hidden: {}, type: {}\n", .{ ignore_hidden, @TypeOf(ignore_hidden) });
 
     // TODO: experiment with comptime stuff
 }
 
-fn myFunc() struct{a: i8, b: u8} {
-    return .{.a = 8, .b = 8};
+fn myFunc() struct { a: i8, b: u8 } {
+    return .{ .a = 8, .b = 8 };
 }
 
-fn parsei8(str: []const u8) !i8 {
-    return std.fmt.parseInt(i8, str, 10);
+fn parsei8(str: ?[]const u8) !i8 {
+    // treat ? as null string
+    const final_str = str orelse "";
+    return std.fmt.parseInt(i8, final_str, 10);
 }
 
-fn failedParsei8(str: []const u8) !i8 {
+fn failedParsei8(str: ?[]const u8) !i8 {
     _ = str;
     return error.ParseError;
 }
 
+fn parseBool(str: ?[]const u8) !bool {
+    // set value to true when no value is provided
+    // e.g. ./program_name --debug
+    //                              => debug is  set to true
+    const bool_str = str orelse {
+        return true;
+    };
+
+    return ((std.mem.eql(u8, bool_str, @as([]const u8, "0"))) and
+        (std.mem.eql(u8, bool_str, @as([]const u8, "false"))) and
+        (std.mem.eql(u8, bool_str, @as([]const u8, "FALSE"))));
+}
+
 test "lib single args" {
-    const num_flag = lib.Flag(i8){.name = "number", .alias = "num", .parse_func = parsei8};
-    var args = [_][*:0]const u8{"--number", "117"};
+    const num_flag = lib.Flag(i8){ .name = "number", .alias = "num", .parse_func = parsei8 };
+    var args = [_][*:0]const u8{ "--number", "117" };
     const kv = try lib.keyValueFromArgs(std.heap.page_allocator, &args);
     var value = num_flag.valueFromMap(kv) catch unreachable;
     try expect(value.? == 117);
 }
 
 test "lib double args" {
-    const num_flag = lib.Flag(i8){.name = "number", .alias = "num", .parse_func = parsei8};
-    const line_flag = lib.Flag(i8){.name = "line", .parse_func = parsei8};
+    const num_flag = lib.Flag(i8){ .name = "number", .alias = "num", .parse_func = parsei8 };
+    const line_flag = lib.Flag(i8){ .name = "line", .parse_func = parsei8 };
 
-    var args = [_][*:0]const u8{"--number", "117", "-line", "85"};
+    var args = [_][*:0]const u8{ "--number", "117", "-line", "85" };
     const kv = try lib.keyValueFromArgs(std.heap.page_allocator, &args);
     var value1 = num_flag.valueFromMap(kv) catch unreachable;
     try expect(value1.? == 117);
@@ -71,23 +86,37 @@ test "lib double args" {
 }
 
 test "lib single alias" {
-    const num_flag = lib.Flag(i8){.name = "number", .alias = "num", .parse_func = parsei8};
-    var args = [_][*:0]const u8{"--num", "117"};
+    const num_flag = lib.Flag(i8){ .name = "number", .alias = "num", .parse_func = parsei8 };
+    var args = [_][*:0]const u8{ "--num", "117" };
     const kv = try lib.keyValueFromArgs(std.heap.page_allocator, &args);
     var value = num_flag.valueFromMap(kv) catch unreachable;
     try expect(value.? == 117);
 }
 
 test "lib no args" {
-    const num_flag = lib.Flag(i8){.name = "number", .alias = "num", .parse_func = parsei8};
+    const num_flag = lib.Flag(i8){ .name = "number", .alias = "num", .parse_func = parsei8 };
     var args = [_][*:0]const u8{};
     const kv = try lib.keyValueFromArgs(std.heap.page_allocator, &args);
     var value = num_flag.valueFromMap(kv) catch unreachable;
     try expect(value == null);
 }
 
+test "lib args with boolean args" {
+    const is_dead = lib.Flag(bool){ .name = "is_dead", .parse_func = parseBool };
+    const is_lucky = lib.Flag(bool){ .name = "is_lucky", .parse_func = parseBool };
+    const is_happy = lib.Flag(bool){ .name = "is_happy", .parse_func = parseBool };
+    var args = [_][*:0]const u8{ "--is_dead", "--is_lucky", "false", "--is_happy" };
+    const kv = try lib.keyValueFromArgs(std.heap.page_allocator, &args);
+    var is_dead_value = is_dead.valueFromMap(kv) catch unreachable;
+    try expect(is_dead_value.? == true);
+    var is_lucky_value = is_lucky.valueFromMap(kv) catch unreachable;
+    try expect(is_lucky_value.? == false);
+    var is_happy_value = is_happy.valueFromMap(kv) catch unreachable;
+    try expect(is_happy_value.? == true);
+}
+
 test "lib with default" {
-    const num_flag = lib.Flag(i8){.name = "number", .alias = "num", .parse_func = parsei8, .default = 88};
+    const num_flag = lib.Flag(i8){ .name = "number", .alias = "num", .parse_func = parsei8, .default = 88 };
     var args = [_][*:0]const u8{};
     const kv = try lib.keyValueFromArgs(std.heap.page_allocator, &args);
     var value = num_flag.valueFromMap(kv) catch unreachable;
@@ -95,15 +124,15 @@ test "lib with default" {
 }
 
 test "lib with erronous parsing " {
-    const num_flag = lib.Flag(i8){.name = "number", .alias = "num", .parse_func = failedParsei8, .default = 88};
-    var args = [_][*:0]const u8{"--num", "125"};
+    const num_flag = lib.Flag(i8){ .name = "number", .alias = "num", .parse_func = failedParsei8, .default = 88 };
+    var args = [_][*:0]const u8{ "--num", "125" };
     const kv = try lib.keyValueFromArgs(std.heap.page_allocator, &args);
     try expectError(error.ParseError, num_flag.valueFromMap(kv));
 }
 
 test "lib with both named and alias parsing " {
-    const num_flag = lib.Flag(i8){.name = "number", .alias = "num", .parse_func = failedParsei8, .default = 88};
-    var args = [_][*:0]const u8{"--num", "125", "--number", "126"};
+    const num_flag = lib.Flag(i8){ .name = "number", .alias = "num", .parse_func = failedParsei8, .default = 88 };
+    var args = [_][*:0]const u8{ "--num", "125", "--number", "126" };
     const kv = try lib.keyValueFromArgs(std.heap.page_allocator, &args);
     try expectError(error.AmbiguousValue, num_flag.valueFromMap(kv));
 }
